8859e747f88ed2b6422d07a560a80fabb91e6e6a
Sanjay <sanjaykumar.parmar21@gmail.com>
2021-08-25 21:11:32 +0530

    Removed message from required validation

--- resources/views/emails/contact_us.blade.php
+++ resources/views/emails/contact_us.blade.php
@@ -4,8 +4,9 @@ Contact Us form details:<br>
 First Name: {{$contactUs->first_name}}<br>
 Last Name: {{$contactUs->last_name}}<br>
 Email: {{$contactUs->email}}<br>
+@if($contactUs->message)
 Message: {{$contactUs->message}}<br>
-
+@endif
 <br>
 Thank You <br>
 Vurg
